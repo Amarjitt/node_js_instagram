04dd35aaa36493140ab4c886c682440f66186a41
Amarjitt <amarjeet2020singh@gmail.com>
2021-04-03 22:20:59 +0530

    some files deleted from activity

--- activity/app/router/homeRouter.js
+++
@@ -1,7 +0,0 @@
-const express = require('express'); 
-const homeRouter = new express.Router();
-
-let {getHomePage } = require('../controller/homeController');
-
-homeRouter.route("/api/v1/").get(getHomePage);
-module.exports = homeRouter;
